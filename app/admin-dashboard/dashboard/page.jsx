486b212b0e5a2894a82eabf2693953f07cc0f204
Danielth999 <kasuto9481@gmail.com>
2024-05-29 10:37:34 +0700

    refactor: Add client-side session validation in DashboardPage component
    The DashboardPage component was updated to include client-side session validation. The useSession hook from the next-auth/react package was used to retrieve the user session data. If the session is still loading, the component waits for it to finish loading. Once the session is loaded, the code checks if the user is logged in and has the "admin" role. If not, the user is redirected to the homepage. This change ensures that only users with the "admin" role can access the dashboard page.
    
    Refactor DashboardPage component to include client-side session validation

--- app/admin-dashboard/dashboard/page.jsx
+++ app/admin-dashboard/dashboard/page.jsx
@@ -1,6 +1,18 @@
+'use client'
 import React from "react";
 import Dashboard from "../../../components/admin-dashboard/dashboard/Dashboard";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 const DashboardPage = () => {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+  useEffect(() => {
+    if (status === "loading") return; // รอให้ session โหลดเสร็จ
+    if (!session || session.user.role !== "admin") {
+      router.push("/"); // redirect ไปยังหน้าแรกถ้าไม่มีสิทธิ์
+    }
+  }, [session, status, router]);
   return (
     <>
       <Dashboard />
