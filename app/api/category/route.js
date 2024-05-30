9a0d2c5cb2c70c5729de40ef97eb0a57d2d4ce65
Danielth999 <kasuto9481@gmail.com>
2024-05-30 15:37:02 +0700

    refactor: Update category route to optimize database query
    The category route was updated to optimize the database query by removing unnecessary code. The findMany method in the Prisma client was modified to remove an empty object parameter. This change simplifies the code and improves performance.
    
    Refactor category route to optimize database query

--- app/api/category/route.js
+++ app/api/category/route.js
@@ -1,15 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
-import translate from "@vitalets/google-translate-api";
-import slugify from "slugify";
 
 const prisma = new PrismaClient();
 
 export const GET = async () => {
   try {
-    const categories = await prisma.category.findMany({
-   
-    });
+    const categories = await prisma.category.findMany({});
     return NextResponse.json(categories, { status: 200 });
   } catch (error) {
     console.error("Error fetching categories:", error);
