6a0a1f2ba6a665cd03accc0b14e7383e97f1a0d3
Danielth999 <kasuto9481@gmail.com>
2024-05-31 13:27:47 +0700

    fix productList and UserList

--- components/admin-dashboard/product/ProductList.jsx
+++ components/admin-dashboard/product/ProductList.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useState, useEffect, Suspense } from "react";
 import axios from "axios";
 import { useRouter, useSearchParams } from "next/navigation";
 import { Pencil, Trash } from "lucide-react";
@@ -74,10 +74,6 @@ const ProductList = () => {
     setSelectedProduct(product);
   };
 
-  useEffect(() => {
-    setCurrentPage(1); // Reset page to 1 whenever searchTerm changes
-  }, [searchTerm]);
-
   // Logic for displaying products
   const indexOfLastProduct = currentPage * productsPerPage;
   const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
@@ -93,8 +89,6 @@ const ProductList = () => {
     indexOfLastProduct
   );
 
-  const totalPages = Math.ceil(filteredProducts.length / productsPerPage);
-
   // Change page
   const paginate = (pageNumber) => {
     setCurrentPage(pageNumber);
@@ -108,85 +102,83 @@ const ProductList = () => {
           <Spinner />
         </div>
       ) : (
-        <Card className="bg-base-100 shadow-xl">
-          <CardHeader>
-            <CardTitle>รายการผลิตภัณฑ์</CardTitle>
-            <div className="flex items-center justify-between">
+        <Suspense fallback={<Spinner />}>
+          <Card className="bg-base-100 shadow-xl">
+            <CardHeader>
+              <CardTitle>รายการผลิตภัณฑ์</CardTitle>
               <CardDescription>ค้นหาและจัดการผลิตภัณฑ์ในระบบ</CardDescription>
-              <ModalAddProduct onProductAdded={fetchProducts} />
-            </div>
-          </CardHeader>
-          <CardContent>
-            <div className="mb-2 flex justify-between">
-            
+            </CardHeader>
+            <CardContent>
+              <div className="mb-2 flex justify-between">
                 <Input
-                  className="w-full"
                   placeholder="ค้นหาผลิตภัณฑ์"
                   value={searchTerm}
                   onChange={(e) => setSearchTerm(e.target.value)}
                 />
-             
-              <div>
-                <Pagination
-                  totalPages={totalPages}
-                  currentPage={currentPage}
-                  onPageChange={paginate}
-                />
+                <ModalAddProduct onProductAdded={fetchProducts} />
               </div>
-            </div>
-
-            <div className="overflow-x-auto">
-              <Table>
-                <TableHeader>
-                  <TableRow>
-                    <TableHead>#ID</TableHead>
-                    <TableHead>รูปภาพ</TableHead>
-                    <TableHead>ชื่อผลิตภัณฑ์</TableHead>
-                    <TableHead>รายละเอียด</TableHead>
-                    <TableHead>ราคา</TableHead>
-                    <TableHead>จัดการ</TableHead>
-                  </TableRow>
-                </TableHeader>
-                <TableBody>
-                  {currentProducts.map((product) => (
-                    <TableRow key={product.productId}>
-                      <TableCell>{product.productId}</TableCell>
-                      <TableCell>
-                        {product.imageUrl ? (
-                          <Image
-                            src={product.imageUrl}
-                            width={100}
-                            height={100}
-                            alt={product.name}
-                          />
-                        ) : (
-                          <span>No Image</span>
-                        )}
-                      </TableCell>
-                      <TableCell>{product.name}</TableCell>
-                      <TableCell>{product.description}</TableCell>
-                      <TableCell>{product.price.toFixed(2)} บาท</TableCell>
-                      <TableCell>
-                        <Button
-                          variant="link"
-                          onClick={() => handleEditClick(product)}
-                        >
-                          <Pencil className="text-blue-500" />
-                        </Button>
-                        <Button
-                          variant="link"
-                          onClick={() => handleDelete(product.productId)}
-                        >
-                          <Trash className="text-red-500" />
-                        </Button>
-                      </TableCell>
+              <div className="overflow-x-auto">
+                <Table>
+                  <TableHeader>
+                    <TableRow>
+                      <TableHead>#ID</TableHead>
+                      <TableHead>รูปภาพ</TableHead>
+                      <TableHead>ชื่อผลิตภัณฑ์</TableHead>
+                      <TableHead>รายละเอียด</TableHead>
+                      <TableHead>ราคา</TableHead>
+                      <TableHead>จัดการ</TableHead>
                     </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </div>
-          </CardContent>
-        </Card>
+                  </TableHeader>
+                  <TableBody>
+                    {currentProducts.map((product) => (
+                      <TableRow key={product.productId}>
+                        <TableCell>{product.productId}</TableCell>
+                        <TableCell>
+                          {product.imageUrl ? (
+                            <Image
+                              src={product.imageUrl}
+                              width={100}
+                              height={100}
+                              alt={product.name}
+                            />
+                          ) : (
+                            <span>No Image</span>
+                          )}
+                        </TableCell>
+                        <TableCell>{product.name}</TableCell>
+                        <TableCell>{product.description}</TableCell>
+                        <TableCell>{product.price.toFixed(2)} บาท</TableCell>
+                        <TableCell>
+                          <Button
+                            variant="link"
+                            onClick={() => handleEditClick(product)}
+                          >
+                            <Pencil className="text-blue-500" />
+                          </Button>
+                          <Button
+                            variant="link"
+                            onClick={() => handleDelete(product.productId)}
+                          >
+                            <Trash className="text-red-500" />
+                          </Button>
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </div>
+            </CardContent>
+            <CardFooter className="flex justify-center mt-4">
+              <Pagination
+                totalPages={Math.ceil(
+                  filteredProducts.length / productsPerPage
+                )}
+                currentPage={currentPage}
+                onPageChange={paginate}
+              />
+            </CardFooter>
+          </Card>
+        </Suspense>
       )}
       {selectedProduct && (
         <ModalEditProduct
--- components/admin-dashboard/users/UserList.jsx
+++ components/admin-dashboard/users/UserList.jsx
@@ -1,11 +1,11 @@
-"use client";
-import { useState, useEffect } from "react";
-import axios from "axios";
-import { useRouter, useSearchParams } from "next/navigation";
-import { Pencil, Trash } from "lucide-react";
-import Spinner from "../../spinner/Spinner";
-import ModalAddUser from "./ModalAddUser";
-import ModalEditUser from "./ModalEditUser";
+'use client';
+import { useState, useEffect, Suspense } from 'react';
+import axios from 'axios';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { Pencil, Trash } from 'lucide-react';
+import Spinner from '../../spinner/Spinner';
+import ModalAddUser from './ModalAddUser';
+import ModalEditUser from './ModalEditUser';
 import {
   Table,
   TableBody,
@@ -14,9 +14,9 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "@/components/ui/table";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
+} from '@/components/ui/table';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
 import {
   Card,
   CardContent,
@@ -24,15 +24,15 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card";
+} from '@/components/ui/card';
 import {
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "@/components/ui/select";
-import Pagination from "../../Pagination"; // นำเข้า Pagination component
+} from '@/components/ui/select';
+import Pagination from '../../Pagination'; // นำเข้า Pagination component
 
 const UserList = () => {
   const router = useRouter();
@@ -41,12 +41,12 @@ const UserList = () => {
   const [loading, setLoading] = useState(true);
   const [currentPage, setCurrentPage] = useState(1);
   const [usersPerPage] = useState(5);
-  const [searchTerm, setSearchTerm] = useState("");
-  const [filterRole, setFilterRole] = useState("all");
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filterRole, setFilterRole] = useState('all');
   const [selectedUser, setSelectedUser] = useState(null);
 
   useEffect(() => {
-    const page = parseInt(searchParams.get("page")) || 1;
+    const page = parseInt(searchParams.get('page')) || 1;
     setCurrentPage(page);
   }, [searchParams]);
 
@@ -58,10 +58,10 @@ const UserList = () => {
       if (res.data && Array.isArray(res.data.users)) {
         setUsers(res.data.users);
       } else {
-        throw new Error("Fetched data is not in the expected format");
+        throw new Error('Fetched data is not in the expected format');
       }
     } catch (error) {
-      console.log("Error fetching users:", error);
+      console.log('Error fetching users:', error);
     } finally {
       setLoading(false);
     }
@@ -76,7 +76,7 @@ const UserList = () => {
       await axios.delete(`${process.env.NEXT_PUBLIC_API_URL}/api/users/${id}`);
       fetchUsers(); // Fetch users again to update the list
     } catch (error) {
-      console.error("Error deleting user:", error);
+      console.error('Error deleting user:', error);
     }
   };
 
@@ -84,17 +84,13 @@ const UserList = () => {
     setSelectedUser(user);
   };
 
-  useEffect(() => {
-    setCurrentPage(1); // Reset page to 1 whenever searchTerm or filterRole changes
-  }, [searchTerm, filterRole]);
-
   // Logic for displaying users
   const indexOfLastUser = currentPage * usersPerPage;
   const indexOfFirstUser = indexOfLastUser - usersPerPage;
 
   const filteredUsers = users.filter((user) => {
     return (
-      (filterRole === "all" ? true : user.roles === filterRole) &&
+      (filterRole === 'all' ? true : user.roles === filterRole) &&
       (user.username.toLowerCase().includes(searchTerm.toLowerCase()) ||
         user.email.toLowerCase().includes(searchTerm.toLowerCase()))
     );
@@ -116,102 +112,109 @@ const UserList = () => {
           <Spinner />
         </div>
       ) : (
-        <Card>
-          <CardHeader>
+        <Suspense fallback={<Spinner />}>
+          <Card>
+            <CardHeader>
               <CardTitle>รายชื่อผู้ใช้</CardTitle>
-            <div className="flex items-center justify-between">
-              <CardDescription>ค้นหาและจัดการผู้ใช้ในระบบ</CardDescription>
-              <div className="flex justify-end">
-                <ModalAddUser onUserAdded={fetchUsers} />
+              <div className="flex items-center justify-between">
+                <CardDescription>ค้นหาและจัดการผู้ใช้ในระบบ</CardDescription>
+                <div className="flex justify-end">
+                  <ModalAddUser onUserAdded={fetchUsers} />
+                </div>
               </div>
-            </div>
-          </CardHeader>
-          <CardContent>
-            <div className="mb-2 flex justify-between items-center">
-              <div className="flex space-x-2">
-                <Input
-                  placeholder="ค้นหา"
-                  value={searchTerm}
-                  onChange={(e) => setSearchTerm(e.target.value)}
-                />
-                <Select
-                  value={filterRole}
-                  onValueChange={(value) => setFilterRole(value)}
-                >
-                  <SelectTrigger className="w-[180px]">
-                    <SelectValue placeholder="กรอง" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectItem value="all">ทั้งหมด</SelectItem>
-                    <SelectItem value="admin">Admin</SelectItem>
-                    <SelectItem value="user">User</SelectItem>
-                  </SelectContent>
-                </Select>
-                <Button
-                  className="bg-[#204d9c] text-white"
-                  onClick={() => router.push(`?page=1`)}
-                >
-                  ค้นหา
-                </Button>
+            </CardHeader>
+            <CardContent>
+              <div className="mb-2 flex justify-between items-center">
+                <div className="flex space-x-2">
+                  <Input
+                    placeholder="ค้นหา"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                  />
+                  <Select
+                    value={filterRole}
+                    onValueChange={(value) => setFilterRole(value)}
+                  >
+                    <SelectTrigger className="w-[180px]">
+                      <SelectValue placeholder="กรอง" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="all">ทั้งหมด</SelectItem>
+                      <SelectItem value="admin">Admin</SelectItem>
+                      <SelectItem value="user">User</SelectItem>
+                    </SelectContent>
+                  </Select>
+                  <Button className="bg-[#204d9c] text-white">ค้นหา</Button>
+                </div>
+
+                <div className="">
+                  <Pagination
+                    totalPages={totalPages}
+                    currentPage={currentPage}
+                    onPageChange={paginate}
+                  />
+                </div>
               </div>
-              <div className="">
+              <div className="overflow-x-auto">
+                <Table>
+                  <TableCaption>รายชื่อผู้ใช้ในระบบ</TableCaption>
+                  <TableHeader>
+                    <TableRow>
+                      <TableHead>#ID</TableHead>
+                      <TableHead>ชื่อผู้ใช้</TableHead>
+                      <TableHead>อีเมล</TableHead>
+                      <TableHead>สิทธิ์การใช้งาน</TableHead>
+                      <TableHead>จัดการ</TableHead>
+                    </TableRow>
+                  </TableHeader>
+                  <TableBody>
+                    {Array.isArray(currentUsers) && currentUsers.length > 0 ? (
+                      currentUsers.map((user) => (
+                        <TableRow key={user.userId}>
+                          <TableCell>{user.userId}</TableCell>
+                          <TableCell>{user.username}</TableCell>
+                          <TableCell>{user.email}</TableCell>
+                          <TableCell>{user.roles}</TableCell>
+                          <TableCell>
+                            <Button
+                              onClick={() => handleEditClick(user)}
+                              variant="link"
+                              className="text-blue-500"
+                            >
+                              <Pencil />
+                            </Button>
+                            <Button
+                              onClick={() => handleDelete(user.userId)}
+                              variant="link"
+                              className="text-red-500"
+                            >
+                              <Trash />
+                            </Button>
+                          </TableCell>
+                        </TableRow>
+                      ))
+                    ) : (
+                      <TableRow>
+                        <TableCell colSpan="5" className="text-center">
+                          ไม่พบผู้ใช้
+                        </TableCell>
+                      </TableRow>
+                    )}
+                  </TableBody>
+                </Table>
+              </div>
+            </CardContent>
+            <CardFooter>
+              <div className="flex justify-center mt-4">
                 <Pagination
                   totalPages={totalPages}
                   currentPage={currentPage}
                   onPageChange={paginate}
                 />
               </div>
-            </div>
-            <div className="overflow-x-auto">
-              <Table>
-                <TableCaption>รายชื่อผู้ใช้ในระบบ</TableCaption>
-                <TableHeader>
-                  <TableRow>
-                    <TableHead>#ID</TableHead>
-                    <TableHead>ชื่อผู้ใช้</TableHead>
-                    <TableHead>อีเมล</TableHead>
-                    <TableHead>สิทธิ์การใช้งาน</TableHead>
-                    <TableHead>จัดการ</TableHead>
-                  </TableRow>
-                </TableHeader>
-                <TableBody>
-                  {Array.isArray(currentUsers) && currentUsers.length > 0 ? (
-                    currentUsers.map((user) => (
-                      <TableRow key={user.userId}>
-                        <TableCell>{user.userId}</TableCell>
-                        <TableCell>{user.username}</TableCell>
-                        <TableCell>{user.email}</TableCell>
-                        <TableCell>{user.roles}</TableCell>
-                        <TableCell>
-                          <Button
-                            onClick={() => handleEditClick(user)}
-                            variant="link"
-                            className="text-blue-500"
-                          >
-                            <Pencil />
-                          </Button>
-                          <Button
-                            onClick={() => handleDelete(user.userId)}
-                            variant="link"
-                            className="text-red-500"
-                          >
-                            <Trash />
-                          </Button>
-                        </TableCell>
-                      </TableRow>
-                    ))
-                  ) : (
-                    <TableRow>
-                      <TableCell colSpan="5" className="text-center">
-                        ไม่พบผู้ใช้
-                      </TableCell>
-                    </TableRow>
-                  )}
-                </TableBody>
-              </Table>
-            </div>
-          </CardContent>
-        </Card>
+            </CardFooter>
+          </Card>
+        </Suspense>
       )}
       {selectedUser && (
         <ModalEditUser user={selectedUser} onUserUpdated={fetchUsers} />
