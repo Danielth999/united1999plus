7df7a7dd29214b297cef72d455821a1ed89fb659
Danielth999 <kasuto9481@gmail.com>
2024-06-06 15:33:33 +0700

    refactor: Update grid layout in Category, CleaningProducts, OfficeSupplies, and Packaging components

--- app/api/category/route.js
+++ app/api/category/route.js
@@ -1,3 +1,8 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
 export const GET = async (request) => {
   try {
     const { searchParams } = new URL(request.url);
