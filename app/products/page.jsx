995ae81cd04e29436eeef0d77964062576821bc4
Danielth999 <kasuto9481@gmail.com>
2024-06-10 15:29:09 +0700

    แก้ไขตรง Import componet ให้ชือเป็นพิมพ์ใหญ่

--- app/products/page.jsx
+++ app/products/page.jsx
@@ -15,7 +15,7 @@ import {
 } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import useSWR from "swr";
-import Pagination from "@/components/pagination";
+import Pagination from "@/components/Pagination";
 
 const fetcher = (url) => axios.get(url).then((res) => res.data);
 
